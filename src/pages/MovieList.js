79dc2c919aa96512c1af6c6b7ed808b0a2cef5ee
CryptoChris <c.peters@makermaker.de>
2021-12-22 12:06:44 +0100

    included first simple searchbar

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
-import MovieItem from '../Components/MovieItem'
-import MovieApi from '../api/MovieApi.js'
+import MovieItem from "../Components/MovieItem"
+import MovieApi from "../api/MovieApi.js"
+import Nav from "../Components/Nav"
+import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 
-
-
-
-class MovieList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
-    }
-
-    componentDidMount() {
+const MovieList = () => {
+    const [data, setData] = useState([])
+    let location = useLocation()
+    useEffect(() => {
         let test = async () => {
-            this.setState({ data: await MovieApi.getPopularMovies() })
+            console.log(location.state)
+            if (location.state == null) {
+                let result = await MovieApi.getPopularMovies()
+                // setData(await MovieApi.getPopularMovies())
+                setData(result)
+            } else {
+                setData(location.state)
+            }
         }
         test()
-
-    }
-    render() {
-        return (
+    })
+    return (
+        <div>
+            <Nav />
             <div className="grid">
-                {this.state.data.map((elt, i) => (
+                {data.map((elt, i) => (
                     <MovieItem
                         img={elt}
                         key={i}
@@ -36,11 +37,47 @@ class MovieList extends Component {
                     />
                 ))}
             </div>
-        );
-    }
+        </div>
+    )
 }
 
-export default MovieList;
+export default MovieList
 
+// class MovieList extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             data: [],
+//         }
+//     }
 
+//     componentDidMount() {
+//         let test = async () => {
+//             this.setState({ data: await MovieApi.getPopularMovies() })
+//         }
+//         test()
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <Nav />
+//                 <div className="grid">
+//                     {this.state.data.map((elt, i) => (
+//                         <MovieItem
+//                             img={elt}
+//                             key={i}
+//                             id={elt.id}
+//                             vote_average={elt.vote_average}
+//                             poster_path={elt.poster_path}
+//                             release_date={elt.release_date}
+//                             genre_ids={elt.genre_ids}
+//                             title={elt.title}
+//                         />
+//                     ))}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
+// export default MovieList
