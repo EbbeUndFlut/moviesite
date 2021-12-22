074a66ad31f3c64aa85c080793651d9dc5dabd30
AkhimBari <akhim.bari91@gmail.com>
2021-12-22 14:07:40 +0100

    bug

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MovieList from "./pages/MovieList";
 import MovieDetail from "./pages/MovieDetail";
 import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
+import React, { Component } from 'react';
 import './App.scss';
 
 
@@ -17,8 +19,11 @@ const App = () => {
                     <Route path="/:id" element={<MovieDetail />} />
                 </Routes>
             </Router>
+            <Footer />
         </div>
     );
 }
 
 export default App;
+
+
